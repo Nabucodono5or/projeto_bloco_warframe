ba9de1224a7dc431768c6363b058ea7d14e21d5d
Jefferson Abreu <nabucodonosor310@gmail.com>
2020-02-21 09:42:00 -0300

    instaladas e inseridas as dependencias do angular material

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
-import angular from 'angular';
-import componentModule from './components/component';
-import appComponent from './app.component';
-import uiRouter from '@uirouter/angularjs';
+import angular from "angular";
+import componentModule from "./components/component";
+import appComponent from "./app.component";
+import uiRouter from "@uirouter/angularjs";
+import ngMaterial from "angular-material";
+import ngMessages from "angular-messages";
+import ngAria from "angular-aria";
+import ngAnimate from "angular-animate";
 
 angular
-  .module('app', [componentModule, uiRouter])
-  .component('app', appComponent)
-  .name;
+  .module("app", [
+    componentModule,
+    uiRouter,
+    ngMessages,
+    ngAria,
+    ngAnimate,
+    ngMaterial
+  ])
+  .component("app", appComponent).name;
