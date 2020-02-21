ca26d45169a64e3a9a07d1bf8f5f855277bda8ba
Jefferson Abreu <nabucodonosor310@gmail.com>
2020-02-21 12:28:13 -0300

    Criado sidenav com material library

--- src/components/toolbar/toolbar.controller.js
+++ src/components/toolbar/toolbar.controller.js
@@ -1,6 +1,15 @@
-let toolbarController = function toolbarController() {
+let toolbarController = function toolbarController($mdSidenav) {
   let vm = this;
   vm.title = "toolbar componente";
+  vm.openLeftMenu = openLeftMenu;
+
+  ////////////////////////////////
+
+  function openLeftMenu() {
+    $mdSidenav('left').toggle();
+  }
 };
+
+toolbarController.$inject = ["$mdSidenav"];
 
 export default toolbarController;
