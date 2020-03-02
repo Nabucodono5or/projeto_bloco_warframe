9bba787af871461421f2ad20f2d3cf2736305b61
Jefferson Abreu <nabucodonosor310@gmail.com>
2020-03-02 11:08:13 -0300

    lidando com a propriedade color

--- src/components/profile/profile.controller.js
+++ src/components/profile/profile.controller.js
@@ -2,11 +2,14 @@ let profileController = function toolbarController($stateParams) {
   let vm = this;
   vm.item = $stateParams.item;
   vm.$onInit = onInit; 
+  vm.color = "#" + vm.item.color;
 
   ////////////////////////////////
 
   function onInit() {
     console.log(vm.item);
+    console.log(vm.color);
+    
   }
 };
 
