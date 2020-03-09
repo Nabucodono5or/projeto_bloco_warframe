b054e76e56e906ff1ac891fa62e6b1011404eb62
Jefferson Abreu <nabucodonosor310@gmail.com>
2020-03-09 16:33:11 -0300

    criado componente rarity

--- src/components/rarity/rarity.service.js
+++ src/components/rarity/rarity.service.js
@@ -1,10 +1,10 @@
 function rarityFactory() {
   let rarities = {
-    'Common': { back: "rgb(255, 51, 51)", color: "white" },
-    'Uncommon': { back: "rgb(255, 51, 51)", color: "white" },
-    'Rare': { back: "rgb(255, 51, 51)", color: "white" },
-    'Legendary': { back: "rgb(255, 51, 51)", color: "white" },
-    'Riven': { back: "rgb(255, 51, 51)", color: "white" }
+    'Common': { back: "rgb(255, 153, 0)", color: "white" },
+    'Uncommon': { back: "rgb(89, 89, 89)", color: "white" },
+    'Rare': { back: "rgb(255, 255, 0)", color: "black" },
+    'Legendary': { back: "rgb(153, 153, 153)", color: "white" },
+    'Riven': { back: "rgb(204, 0, 102)", color: "white" }
   };
 
   let service = {
