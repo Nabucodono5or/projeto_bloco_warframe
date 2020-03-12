49333a9a818046958a662b3eeb995502a64e644d
Jefferson Abreu <nabucodonosor310@gmail.com>
2020-03-11 23:11:12 -0300

    convertido tempo de buildTime para minutes

--- src/components/descricao/descricao.controller.js
+++ src/components/descricao/descricao.controller.js
@@ -7,6 +7,7 @@ let descricaoController = function DescricaoController() {
     function onInit() {
         vm.description = vm.item.description || "Desconhecido";
         vm.buildTime = vm.item.buildTime || 0;
+        vm.minutesBuild = Math.floor(vm.buildTime / 60) + "min";
         vm.buildPrice = vm.item.buildPrice || 0;
         vm.masteryReq = vm.item.masteryReq || 0;
         vm.type = vm.item.type || "Desconhecido";
